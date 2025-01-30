2a71909ba2b91bade30d3f5892351ab92e8bd002
anmolmadaan368 <43415604918anmolmadaan@gmail.com>
2025-01-30 19:17:29 +0530

    Writing auth middlewares

--- src/app.js
+++ src/app.js
@@ -22,38 +22,56 @@ const app = express()
 //     res.send({firstname:"Anmol",lastname:"Madaan"})
 // })
 
-app.use("/user",
-    
-    [(req,res,next)=>{ 
-   // res.send("Response") //Here if we don't use any thing or just use console.log then request will be sended to server infinite times and you will not get any response
-    console.log("1st Route");
-    
-    next();
-    //res.send("Response")
-},
-(req,res,next)=>{
-    console.log("2nd Route");
-    
-    //res.send("2nd Response")
-    next();
-},],
-(req,res,next)=>{
-    console.log("3rd Route");
-    //res.send("3rd Response")
-    next();
-},
-(req,res,next)=>{
-    console.log("4th Route");
-    //res.send("4th Response")
-    next();
-},
-(req,res,next)=>{
-    console.log("5th Route");
-    res.send("5th Response")
-},
-)
+//GET/users => middleware chain => request handler
 
-app.listen(3001,()=>{
+// app.use("/",(req,res,next)=>{
+//     res.send("Handling route")
+// })
+
+// app.get("/user",
+    
+//     (req,res,next)=>{ 
+//    // res.send("Response") //Here if we don't use any thing or just use console.log then request will be sended to server infinite times and you will not get any response
+//     console.log("1st Route");
+    
+//     next();
+//     //res.send("Response")
+// },
+// (req,res,next)=>{
+//     console.log("2nd Route");
+    
+//     //res.send("2nd Response")
+//     next();
+// },
+// (req,res,next)=>{
+//     console.log("3rd Route");
+//     //res.send("3rd Response")
+//     next();
+// },
+// (req,res,next)=>{
+//     console.log("4th Route");
+//     //res.send("4th Response")
+//     next();
+// },
+// (req,res,next)=>{
+//     console.log("5th Route");
+//     res.send("5th Response")
+// },
+// )
+
+const {adminAuth} = require("./middlewares/auth")
+
+app.use("/admin",adminAuth)
+
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data sent")
+})
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("User deleted")
+})
+
+app.listen(3001,(req,res)=>{
     console.log("Successfully running on port 3001");
     
 })
